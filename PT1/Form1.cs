010f37c33654370a5bac389e40573cd2585add5b
Addy <addyrockx96@gmail.com>
2018-04-22 18:24:31 +0530

    Imported

---
+++ PT1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PT1
+{
+    public partial class Form1 : Form
+    {
+        public static String Fname, Lname, Gender = "",Branch = "",AOI;
+        public static Double Price = 2000;
+
+        public Form1()
+
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void checkBox4_CheckedChanged(object sender, EventArgs e) { }
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+        //User Name
+            Fname = textBox1.Text;
+            Lname = textBox2.Text;
+
+                    //Gender
+            bool x = radioButton1.Checked;
+            if (x)
+                Gender = radioButton1.Text;
+            else
+                Gender = radioButton2.Text;
+            
+        //Branch
+            if (radioButton3.Checked)
+                Branch = radioButton3.Text;
+            else if (radioButton4.Checked)
+                Branch = radioButton4.Text;
+            else
+                Branch = radioButton5.Text;
+            
+        //Area of Interest
+            AOI = comboBox1.ToString();
+            
+        //Additional Features
+            if (checkBox1.Checked)
+                Price = Price + 1000;
+            if (checkBox2.Checked)
+                Price = Price + 200;
+            if (checkBox3.Checked)
+                Price = Price + 200;
+            if (checkBox4.Checked)
+                Price = Price + 100;
+            
+            Form2 f1 = new Form2();
+            f1.Show();
+
+
+
+        }
+    }
+}
---
+++ PT1/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PT1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //Form1 f1 = new Form1();
+            label1.Text = Form1.Fname;
+            label2.Text = Form1.Lname;
+            label3.Text = Form1.Branch;
+            label4.Text = Form1.Gender;
+            label5.Text = Form1.AOI;
+            label6.Text = Form1.Price.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
